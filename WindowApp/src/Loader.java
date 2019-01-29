cd6a0c5c26946fb4b9c519f080ae74fa81306c2f
vadim <ivadimn@mail.ru>
2019-01-29 16:40:16 +0300

    1 module finish

---
+++ WindowApp/src/Loader.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Loader {
+    private static final int WIDTH = 800;
+    private static final int HEGHT = 600;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        //Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(WIDTH, HEGHT);
+        frame.setLocationByPlatform(true);
+        /*frame.setBounds(dimension.width / 2 - WIDTH / 2,
+                 dimension.height / 2 - HEGHT / 2,
+                WIDTH, HEGHT);*/
+        frame.setTitle("First Window Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
