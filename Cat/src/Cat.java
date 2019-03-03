47c8326c37dc7266e54cadd0729902431dcc524b
ivadimn <ivadimn@mail.ru>
2019-03-03 21:45:14 +0300

    new version Cat

--- Cat/src/Cat.java
+++ Cat/src/Cat.java
@@ -1,11 +1,18 @@
 
 public class Cat
 {
+    private static Integer count = 0;
+
     private Double originWeight;
     private Double weight;
 
     private Double minWeight;
     private Double maxWeight;
+    private Double massFeed = 0.0;
+
+    public static Integer getCatCount() {
+        return count;
+    }
 
     public Cat()
     {
@@ -13,7 +20,16 @@ public class Cat
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        count++;
+    }
 
+    public Cat(Double weight)
+    {
+        this.weight = weight;
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+        count++;
     }
 
     public void meow()
@@ -22,9 +38,19 @@ public class Cat
         //System.out.println("Meow");
     }
 
+    public void gotoWC() {
+        Double wc = 100.0 * Math.random();
+        weight = weight - wc;
+        System.out.println("Go to WC weight - " + wc);
+    }
+
+    public Double getMassFeed() {
+        return massFeed;
+    }
     public void feed(Double amount)
     {
         weight = weight + amount;
+        massFeed = massFeed + amount;
     }
 
     public void drink(Double amount)
@@ -40,9 +66,11 @@ public class Cat
     public String getStatus()
     {
         if(weight < minWeight) {
+            count--;
             return "Dead";
         }
         else if(weight > maxWeight) {
+            count--;
             return "Exploded";
         }
         else if(weight > originWeight) {
--- Cat/src/Loader.java
+++ Cat/src/Loader.java
@@ -7,15 +7,25 @@ public class Loader
         int catsCount = 7;
         Cat[] cats = new Cat[catsCount];
         for (int i = 0; i < catsCount; i++) {
-            cats[i] = new Cat();
+            cats[i] = getCittent();
             System.out.println("Weight cat " + i + " - " + cats[i].getWeight());
         }
+
+        System.out.println("Количество кошек - " + Cat.getCatCount());
+
         System.out.println("Накормим и напоем");
         for (int i = 0; i < catsCount; i++) {
             cats[i].feed(5000 * Math.random());
             cats[i].drink(5000 * Math.random());
             System.out.println("Cat " + i + " Weight - " + cats[i].getWeight() + " Status - " + cats[i].getStatus());
         }
+
+        System.out.println("Осталось в живых  - " + Cat.getCatCount());
+
+//        for (int i = 0; i < catsCount; i++) {
+//            cats[i].gotoWC();
+//            System.out.println("Cat " + i + " Weight - " + cats[i].getWeight() + " Status - " + cats[i].getStatus());
+//        }
 
         System.out.println("Мяучим ... ");
         for (int i = 0; i < catsCount; i++) {
@@ -24,5 +34,10 @@ public class Loader
             }
             System.out.println("Cat " + i + " Weight - " + cats[i].getWeight() + " Status - " + cats[i].getStatus());
         }
+        System.out.println("Осталось в живых после мяучинья - " + Cat.getCatCount());
+    }
+
+    public static Cat getCittent() {
+        return new Cat(100 + 100 * Math.random());
     }
 }
