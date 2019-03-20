420a5c5e94b8afd13d15c5765601b98f7065f90b
ivadimn <ivadimn@mail.ru>
2019-03-20 22:59:29 +0300

    new class

---
+++ StringExperiments/src/LoaderRegEx.java
@@ -0,0 +1,7 @@
+public class LoaderRegEx {
+
+    public static void main(String[] args) {
+        String text = "Вася заработал 5300 рублей, Петя -756311 рубля, а Маша - 13000 рублей";
+        System.out.println(text.replaceAll("[^0-9^\\s]", ""));
+    }
+}
