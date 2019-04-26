5ac40d76e0ca790e0e3973752a79e3e9a9746fbd
vadim <ivadimn@mail.ru>
2019-04-26 16:33:54 +0300

    new project

---
+++ Gibddd/src/Loader.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Loader {
+    private static final int[] regions = {1, 2, 102, 3, 4, 5, 6, 7, 8, 9, 10,
+    11, 12, 13, 113, 14, 15, 16, 116, 17, 18, 19, 20, 21, 121, 22, 23, 93, 123,
+    24, 84, 88, 124, 25, 125, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 136,
+    37, 38, 85, 39, 91, 40, 41, 82, 42, 142, 43, 44, 45, 46, 47, 48, 40, 50,
+    90, 150, 190, 51, 52, 152, 53, 54, 154, 55, 56, 57, 58, 59, 81, 159, 60,
+    61, 161, 62, 63, 163, 64, 164, 65, 66, 96, 67, 68, 69, 70, 71, 72, 73, 173,
+    74, 174, 75, 80, 76, 77, 97, 99, 177, 199, 197, 777, 78, 98, 178, 79, 83,
+    86, 87, 89, 94, 95};
+
+    private static final String[] federal = {"EKX99", "EKX177", "EKX97", "XKX77",
+    "AMP97", "AOO77", "BOO77", "MOO77", "COO77", "KOO77", "AKP177, BKP177, EKP177",
+    "KKP177", "EPE177", "AMP77", "BMP77", "KMP77", "MMP77", "TMP77", "PMP77", "AMO77",
+    "OMP77", "YMP77"};
+
+    private static ArrayList<String> numbers = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
