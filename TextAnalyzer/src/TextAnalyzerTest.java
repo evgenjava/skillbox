436c0e1dbd8f8b8729e736ee23f0ef724a0df345
ivadimn <ivadimn@mail.ru>
2019-06-01 17:10:40 +0300

    added hamcrest

--- TextAnalyzer/src/TextAnalyzerTest.java
+++ TextAnalyzer/src/TextAnalyzerTest.java
@@ -1,24 +1,38 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class TextAnalyzerTest {
 
     private String testText = "word1 word2? word3 word4, word1 word1, word 23 w question?";
+    private TextAnalyzer ta = new TextAnalyzer(testText);
     @Test
     public void TestWordCount() {
         System.out.println("Test word count");
-        TextAnalyzer ta = new TextAnalyzer(testText);
-        int testWordCount = 9;
-        int actualWordCount = ta.getWords().size();
-        Assert.assertEquals(testWordCount, actualWordCount);
+        int testWordCount = 10;
+        List<String> words = ta.getWords();
+        assertThat(words, hasSize(testWordCount));
+
+       // Assert.assertEquals(testWordCount, actualWordCount);
+    }
+
+    @Test
+    public void TestHasItems() {
+        System.out.println("Test has items");
+        List<String> words = ta.getWords();
+        assertThat(words, hasItems("word1", "23", "w"));
     }
 
     @Test
     public void TestMostFrequentWord() {
         System.out.println("Test most frequent word");
-        TextAnalyzer ta = new TextAnalyzer(testText);
         String testMostFrequentWord = "word1";
         String actualMostFrequentWord = ta.getMostFrequentWord();
-        Assert.assertEquals(testMostFrequentWord, actualMostFrequentWord);
+        assertThat(actualMostFrequentWord, equalTo(testMostFrequentWord));
+        //Assert.assertEquals(testMostFrequentWord, actualMostFrequentWord);
     }
 }
