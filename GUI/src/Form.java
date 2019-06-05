2aff2ab12afb5b8db9842ecf13d7ac44b91da6dc
ivadimn <ivadimn@mail.ru>
2019-06-05 07:16:46 +0300

    new gui form

--- GUI/src/Form.java
+++ GUI/src/Form.java
@@ -2,7 +2,23 @@ import javax.swing.*;
 
 public class Form {
     private JPanel rootPanel;
-    private JTextField textField1;
+    private JTextField txtFamilia;
+    private JLabel lblFamilia;
+    private JPanel pnlFamilia;
+    private JPanel pnlName;
+    private JPanel pnlOtchestvo;
+    private JPanel pnlBirthday;
+    private JPanel pnlCity;
+    private JPanel pnlButton;
+    private JLabel lblName;
+    private JTextField txtName;
+    private JLabel lblOthestvo;
+    private JTextField txtOtchestvo;
+    private JLabel lblBirthday;
+    private JTextField txtBirthday;
+    private JLabel lblCity;
+    private JTextField txtCity;
+    private JButton btnClick;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
